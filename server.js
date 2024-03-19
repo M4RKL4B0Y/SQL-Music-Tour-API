12ae919cb09fad69aed0a36034fbf43cd0a5336c
M4RKL4B0Y <markvlaboy@gmail.com>
2024-03-19 18:30:57 -0400

    edit .env

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
@@ -13,6 +15,17 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+
+// SEQUELIZE CONNECTION
+// const sequelize = new Sequelize(process.env.PG_URI)
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: '${process.env.DB_PASS}'
+  })
+  
 
 // LISTEN
 app.listen(process.env.PORT, () => {
